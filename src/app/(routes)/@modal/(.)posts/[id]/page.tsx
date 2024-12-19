867ba4a6f4ca5b88c847a288eb9358d64e509bee
acedamin325 <daminkhan325@gmail.com>
2024-12-19 21:53:41 +0530

    Version Fixes

--- src/app/(routes)/@modal/(.)posts/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)posts/[id]/page.tsx
@@ -3,14 +3,11 @@ import ModalPostContent from "@/components/ModalPostContent";
 import PreLoader from "@/components/PreLoader";
 import { Suspense } from "react";
 
-export type ParamsType = Promise<{ id: string }>;
-
-export default async function PostInModal(props: { params: ParamsType }) {
-    const { id } = await props.params
+export default async function PostInModal({ params }: { params: { id: string } }) {
     return (
         <Modal>
             <Suspense fallback={<PreLoader />}>
-                <ModalPostContent postId={id} />
+                <ModalPostContent postId={params.id} />
             </Suspense>
         </Modal>
     );
