a5a11911759e1a6ae1599f2f210f522020fd37cc
daminace325 <daminkhan325@gmail.com>
2025-05-20 14:10:35 +0530

    Add additional image domain for optimization

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['turquoise-worldwide-armadillo-75.mypinata.cloud'],
+    domains: [
+      'turquoise-worldwide-armadillo-75.mypinata.cloud',
+      'w7.pngwing.com'
+    ],
   },
 };
 
